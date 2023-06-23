99ab91154e66966ab97ee6d2c4c57075b3447cee
Nezamaev Dmitriy <dnezamaev@gmail.com>
2023-06-23 19:38:09 +0300

    Add project files.

---
+++ SkillboxCsharp05/CommonMistakes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillboxCsharp05
+{
+    /// <summary>
+    /// Ошибка. Использование локальных методов вместо обычных.
+    /// </summary>
+    internal class LocalFunctions
+    {
+        static void MyMain()
+        {
+            // Это локальный метод, его нельзя использовать за пределами метода,
+            // в котором он объявлен.
+            static void SplitText(string text)
+            {
+            }
+        }
+
+        static void OtherMethod()
+        {
+            // Здесь SplitText уже не виден.
+            // SplitText();
+        }
+
+        /// <summary>
+        /// Метод должен быть объявлен на этом уровне.
+        /// </summary>
+        static void SplitTextCorrectLocation()
+        {
+
+        }
+    }
+}
---
+++ SkillboxCsharp05/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SkillboxCsharp05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sampleText = "Hello, my  dear   World!";
+
+            // Пример использования нашего метода разбиения на слова.
+            string[] words = SplitText(sampleText);
+
+            foreach (string word in words) 
+            {
+                Console.WriteLine(word);
+            }
+
+            // Пример использования нашего метода переворота предложения.
+            string reversed = ReverseWords(sampleText);
+            Console.WriteLine();
+            Console.WriteLine(reversed);
+        }
+
+        /// <summary>
+        /// Разбивает предложение на слова, разделяемые проблелами.
+        /// </summary>
+        /// <param name="inputString">Предложение.</param>
+        /// <returns>Массив из слов предложения.</returns>
+        static string[] SplitText(string text)
+        {
+            // StringSplitOptions.RemoveEmptyEntries используется
+            // для удаления пустых записей.
+            // Они могут появиться из-за нескольких идущих подряд пробелов.
+            return text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// Меняет порядок слов в предложении на обратный.
+        /// </summary>
+        /// <param name="text">Предложение.</param>
+        /// <returns>Предложение с обратным порядком слов.</returns>
+        static string ReverseWords(string text)
+        {
+            // Вызываем наш метод разбиения на слова и сохраняем слова в переменной words.
+            string[] words = SplitText(text);
+
+            // Вызываем стандартный метод для переворачивания массива.
+            Array.Reverse(words);
+
+            // Вызываем стандартный метод для склеивания строк и
+            // сохраняем полученное предложение в переменной result.
+            // Первый параметр - разделитель, в нашем случае пробел.
+            // Второй параметр - массив строк для склейки через разделитель.
+            string result = String.Join(' ', words);
+
+            // Возвращаем полученное перевернутое предложение.
+            return result;
+        }
+    }
+}
